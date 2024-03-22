e29a093f6e2d993633259f5925507d1e5b23fadc
Kenny Guo <110429254+kennykguo@users.noreply.github.com>
2024-03-22 12:25:16 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,43 +1,38 @@
-from PyPDF2 import PdfReader 
+from PyPDF2 import PdfReader
 from openai import OpenAI
-#from dotenv import load_dotenv
 import os
 
-
+# Setup OpenAI
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
-
 client = OpenAI(
-   api_key=os.environ.get("OPEN_AI_API_KEY"),
-) 
+    api_key=os.environ.get("OPEN_AI_API_KEY"),
+)
 
-
-# Initalize OpenAI class
-from openai import OpenAI
-client = OpenAI()
-
-# Initalize PDF reader class and get text
+# Initialize PDF reader class and get text
 reader = PdfReader("test.pdf")
 text = ""
+
 # Loops through all pages
 for page in reader.pages:
-   text += page.extract_text()  
+    text += page.extract_text()
 
-# Initalize the prompt
-prompt = '''Your job is now to read through a course syllabus and list all assignment deadlines, midterm exam dates, and any other course deliverables that have due dates, all with any grade weightages. Do not include any extra text. This information is going to be used to create an ics file that the user (students) should be able to use to upload to their preferred calendar application: 
+# Initialize the prompt
+message = """
 
-The syllabus you are using is provided below: '''
+Your job is to read through a course syllabus and list all assignment deadlines, midterm exam dates, and any other course deliverables that have due dates, all with any grade weightages. You should only include the name of the event with grade weightage if available (if not available, just use a hyphen as a filler), the start date formatted in year-month-day, and the end date formatted in year-month-day. For weekly assignments, list each individual assignment and the due date. Do not include and other text than what I have requested. Format as such: Name of Item, Start date, End date, Weightage
 
-sys_prompt = [{"role": "system", "content": prompt}]
-usr_prompt = [{"role": "user", "content": text}]
-model = "gpt-3.5-turbo"
+The syllabus you are using is provided below:
+"""
 
-proceed = input("Proceed? y / n")
+system_prompt = [{"role": "system", "content": message}]
+user_prompt = [{"role": "user", "content": message + text}]
+
+proceed = input("Proceed? y / n: ")
 if proceed == 'y':
-    response = client.chat.completions.create(
-        model = model,
-        messages = sys_prompt + usr_prompt
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=system_prompt + user_prompt
     )
-    
-    print("Done!")
 
-print(response.choices[0].message)
+print(completion.choices[0].message.content)
+print("Done!")
