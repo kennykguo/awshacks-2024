4fd8d5aec581adc0ad1ccf354eade7caddc96bb8
Harsh Patel <92759695+harshhmp@users.noreply.github.com>
2024-03-22 13:11:18 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -43,6 +43,6 @@ def generate_ics():
     return send_file(temp_file, as_attachment=True, attachment_filename='calendar.ics')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
 
