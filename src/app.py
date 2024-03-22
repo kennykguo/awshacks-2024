baff3179d831b8f9630dd217d53905baf1c81302
k-chhajer <106311333+k-chhajer@users.noreply.github.com>
2024-03-22 13:03:55 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, request, send_file
 import os
-import tempfile
 from ics import Calendar, Event
 
 app = Flask(__name__)
@@ -27,26 +26,23 @@ def upload_file():
 
     return "Files uploaded successfully"
 
-@app.route('/download_ics', methods=['POST'])
-def download_ics():
-    if 'pdfFiles' not in request.files:
-        return "No PDF files uploaded."
-
-    pdf_files = request.files.getlist('pdfFiles')
+@app.route('/generate_ics', methods=['GET'])
+def generate_ics():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
     c = Calendar()
 
-    for file in pdf_files:
+    for file in files:
         event = Event()
-        event.name = file.filename
+        event.name = file
         c.events.add(event)
 
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.close()
-
-    with open(temp_file.name, 'w') as f:
+    temp_file = 'calendar.ics'
+    with open(temp_file, 'w') as f:
         f.writelines(c)
 
-    return send_file(temp_file.name, as_attachment=True, attachment_filename='calendar.ics')
+    return send_file(temp_file, as_attachment=True, attachment_filename='calendar.ics')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
